18efaf384bb4079da794c635c8288be3a7bd04bd
Bayramgeldi <bnurberdiyev@colotek.com>
2020-10-11 17:23:49 -0400

    Updates

---
+++ precache-manifest.907d81f382e20c895c48989036c2182b.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "a8e78df88319af7cb46a840366714752",
+    "url": "/bayram96/recipe-app/index.html"
+  },
+  {
+    "revision": "b18fd356f2a3e7a17adf",
+    "url": "/bayram96/recipe-app/static/css/main.6a61b5a9.chunk.css"
+  },
+  {
+    "revision": "a6d1ebf6515c91a53dde",
+    "url": "/bayram96/recipe-app/static/js/2.60e0b9f4.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/bayram96/recipe-app/static/js/2.60e0b9f4.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "b18fd356f2a3e7a17adf",
+    "url": "/bayram96/recipe-app/static/js/main.90562081.chunk.js"
+  },
+  {
+    "revision": "3157d75530bb80e7676e",
+    "url": "/bayram96/recipe-app/static/js/runtime-main.a20a1255.js"
+  }
+]);
